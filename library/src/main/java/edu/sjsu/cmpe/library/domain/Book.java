c14f65fa5044ad1fd0cbd0229623bcea35dcbf26
ParinModi2 <modi.parin@gmail.com>
2014-04-07 09:34:26 -0700

    upload

--- library/src/main/java/edu/sjsu/cmpe/library/domain/Book.java
+++ library/src/main/java/edu/sjsu/cmpe/library/domain/Book.java
@@ -1,5 +1,6 @@
 package edu.sjsu.cmpe.library.domain;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.validation.constraints.NotNull;
@@ -127,5 +128,19 @@ public class Book {
     public void setStatus(Status status) {
 	this.status = status;
     }
+ public void addNewBook (String newBook){
+    	
+    	String[] bookDetails = newBook.replace(":/", "~/").split(":");
+    	this.isbn=Integer.parseInt(bookDetails[0]);
+    	this.title=bookDetails[1];
+    	this.category=bookDetails[2];
+    	String url = bookDetails[3].replace("~/", ":/");
+    	try {
+			this.coverimage=new URL(url);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+    }
+
 
 }
